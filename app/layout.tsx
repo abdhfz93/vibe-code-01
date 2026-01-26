33f2a5f3ff2542ab6e80d1d6433e22c51e55c311
Hafiz Baharuddin <abd.hfz93@gmail.com>
2026-01-26 10:51:51 +0800

    v3.2

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,16 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Nautilus Maintenance Record',
-  description: 'Internal maintenance record management system',
+  title: 'Nautilus Central',
+  description: 'Internal management systems',
+  icons: {
+    icon: [
+      {
+        url: '/favicon.png',
+        href: '/favicon.png',
+      }
+    ],
+  },
 }
 
 export default function RootLayout({
